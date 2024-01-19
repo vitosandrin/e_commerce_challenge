1d1c749bd517c52db596329d861aeec212d7bc94
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 18:28:19 -0300

    feat: add DefaultTheme interface on styled-components module

---
+++ src/style.d.ts
@@ -0,0 +1,65 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      background: string;
+      primary: { 1: string; 2: string };
+      secondary: { 1: string; 2: string };
+      types: {
+        dragon: string;
+        ghost: string;
+        ice: string;
+        water: string;
+        boxShadow: string;
+      };
+      feedback: {
+        warning: string;
+        error: string;
+        success: string;
+        successhover: string;
+        info: string;
+      };
+    };
+    font: {
+      colors: {
+        1: string;
+        2: string;
+        3: string;
+      };
+      sizes: {
+        xxxl: string;
+        xxl: string;
+        xl: string;
+        lg: string;
+        md: string;
+        sm: string;
+        xs: string;
+        xxs: string;
+        xxxs: string;
+      };
+    };
+    breakpoints: {
+      xsm: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+    };
+    transitions: {
+      time: string;
+      type: string;
+    };
+    spacing: {
+      xxxs: string;
+      xxs: string;
+      xs: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      xxl: string;
+      xxxl: string;
+    };
+  }
+}
