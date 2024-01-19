37bc90a81b9a36d46ca074db4e09a4866949b145
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 16:11:14 -0300

    Add product context and provider

---
+++ src/context/product-context.tsx
@@ -0,0 +1,44 @@
+import { createContext, useState, ReactNode } from "react";
+import { Product } from "../entities/models/product";
+import { fakeStoreAPI } from "../config/client/fake-store-api";
+
+interface IProductContext {
+  products: Product[];
+  selectedProduct: Product | null;
+  fetchProducts: () => Promise<void>;
+  selectProduct: (productId: number) => void;
+}
+
+export const ProductContext = createContext<IProductContext>(
+  {} as IProductContext
+);
+
+export const ProductProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const fetchProducts = async () => {
+    const { data: response } = await fakeStoreAPI.get("/products");
+    setProducts(response);
+  };
+
+  const selectProduct = (productId: number) => {
+    const product = products.find((product) => product.id === productId);
+    if (!product) return;
+    setSelectedProduct(product);
+  };
+  
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        selectedProduct,
+        fetchProducts,
+        selectProduct
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
