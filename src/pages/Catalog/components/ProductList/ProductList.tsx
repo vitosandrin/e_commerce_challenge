51332a03186dfc292e0338f6a4f6d6641861f37a
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 17:20:39 -0300

    working: map products on ProductList

--- src/pages/Catalog/components/ProductList/ProductList.tsx
+++ src/pages/Catalog/components/ProductList/ProductList.tsx
@@ -1,22 +1,23 @@
 import { useContext, useEffect } from "react";
 import * as ProductListStyles from "./styles";
 import { ProductContext } from "@src/context/product-context";
+import { ProductCard } from "../ProductCard/ProductCard";
 
 export const ProductList = () => {
   const { fetchProducts, products } = useContext(ProductContext);
 
+  const hasProducts = products.length > 0;
+  console.log(products);
   useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
-    <ProductListStyles.ListContainer
-      align="center"
-      justify="center"
-      direction="row"
-      wrap="wrap"
-    >
-      <h1>{products.length > 0 && products[0].title}</h1>
+    <ProductListStyles.ListContainer>
+      {hasProducts &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
     </ProductListStyles.ListContainer>
   );
 };
