15f3d7285b6206581e7bf524d504f090ae0be7f4
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 16:23:44 -0300

    feat: add default theme StyledComponent

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+export const ResetCss = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    animation: fadeIn ${({ theme }) => theme.transitions.time} ${({ theme }) =>
+  theme.transitions.type};
+    color: ${({ theme }) => theme.font.colors[1]};
+    background-color: ${({ theme }) => theme.colors.background};
+    padding: 0;
+    margin: 0;
+    @media (max-width: ${({ theme }) => theme.breakpoints.xsm}) {
+      font-size: 80%;
+    }
+    overflow-x: hidden;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.typescolor.info};
+    
+    p{
+      color: ${({ theme }) => theme.colors.typescolor.info};
+    }
+  }
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family:"Quicksand";
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    background-color: transparent;
+  }
+  
+  ::-webkit-scrollbar {
+    width: 12px;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    background-color: rgba(0, 0, 0, 0.2);
+
+  }
+  
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(40px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+  }
+`;
---
+++ src/theme/theme.ts
@@ -0,0 +1,70 @@
+export const theme = {
+  colors: {
+    background: "#1c1c1c",
+    primary: {
+      1: "#023b3b",
+      2: "#12adad",
+    },
+    secondary: {
+      1: "#4fe4e4",
+      2: "#edaa29",
+    },
+    types: {
+      water: "#6390F0",
+      ice: "#96D9D6",
+      ghost: "#735797",
+      dragon: "#6F35FC",
+      boxShadow:
+        "rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px",
+    },
+    typescolor: {
+      success: "#007e33",
+      successhover: "#417957",
+      error: "#cc0000",
+      warning: "#ffd900",
+      info: "#3586ff",
+      notAllowed: "#9c9c9c",
+    },
+  },
+  font: {
+    colors: {
+      1: "#FFF",
+      2: "#787886",
+      3: "#000",
+      white: "#FFF",
+    },
+    sizes: {
+      xxxl: "2.5rem",
+      xxl: "2.25rem",
+      xl: "2rem",
+      lg: "1.75rem",
+      md: "1.15rem",
+      sm: "1rem",
+      xs: "0.85rem",
+      xxs: "0.75rem",
+      xxxs: "0.65rem",
+    },
+  },
+  breakpoints: {
+    xsm: "360px",
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1366px",
+  },
+  transitions: {
+    time: "0.3s",
+    type: "ease",
+  },
+  spacing: {
+    xxxs: "4px",
+    xxs: "8px",
+    xs: "16px",
+    sm: "24px",
+    md: "32px",
+    lg: "40px",
+    xl: "48px",
+    xxl: "56px",
+    xxxl: "64px",
+  },
+};
