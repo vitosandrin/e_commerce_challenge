8e95db3291f7ce816bd994bce33aae46300d4b53
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 18:27:35 -0300

    feat: add export Button component on index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { FlexBox } from "./utils/core/FlexBox";
 import { Container } from "./utils/core/Container";
 import { Text } from "./utils/generic/Text";
+import { Button } from "./form/Button/Button";
 
-export { FlexBox, Container, Text };
+export { FlexBox, Container, Text, Button };
