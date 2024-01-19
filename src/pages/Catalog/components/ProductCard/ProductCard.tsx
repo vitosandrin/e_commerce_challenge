ab935ac76b0bc18308ca054d95258f43cf0dc18f
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 18:25:54 -0300

    working product cart

--- src/pages/Catalog/components/ProductCard/ProductCard.tsx
+++ src/pages/Catalog/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,34 @@
-import { Text } from "@src/components";
-import { Card } from "@src/components/utils/core/Card";
+import { Button, FlexBox, Text } from "@src/components";
+import { CartContext } from "@src/context/cart-context";
 import { Product } from "@src/entities/models/product";
-
+import { useContext } from "react";
+import * as ProductCardStyles from "./styles";
+import { formatMonetaryEn } from "@src/utils/object/monetary";
 interface ProductCardProps {
   product: Product;
 }
 export const ProductCard = ({ product }: ProductCardProps) => {
+  const { addToCart } = useContext(CartContext);
   return (
-    <Card>
+    <ProductCardStyles.Card
+      align="center"
+      justify="center"
+      direction="column"
+      gap="xxs"
+    >
       <Text size="medium" weight={500}>
         {product.title}
       </Text>
 
-      <Text size="small">{product.included_at}</Text>
-    </Card>
+      <Text size="small" weight={500}>
+        {formatMonetaryEn(product.price)}
+      </Text>
+
+      <Button colorType="Success" onClick={() => addToCart(product)}>
+        Add to cart
+      </Button>
+
+      <Text size="small">{`Available since: ${product.included_at}`}</Text>
+    </ProductCardStyles.Card>
   );
 };
