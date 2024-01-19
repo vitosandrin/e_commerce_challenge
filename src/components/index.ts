79bf8760a81c51d9d99ab394c7689f2a5f5d99bf
vitosandrin <sandrinvito10@gmail.com>
2024-01-19 16:57:30 -0300

    Add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { FlexBox } from "./utils/core/FlexBox";
+import { Container } from "./utils/core/Container";
+import { Text } from "./utils/generic/Text";
+
+export { FlexBox, Container, Text };
