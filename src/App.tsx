c735a280588bad7c19e162df297053d174d0ae5c
vitosandrin <sandrinvito10@gmail.com>
2024-01-20 00:09:43 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { DefaultRouter } from "./routes/DefaultRouter";
+import { Header } from "@src/components";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <DefaultRouter />
     </BrowserRouter>
   );
