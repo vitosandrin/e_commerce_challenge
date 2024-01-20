1cf7f1f3b5377a17f77685521f16746cdca6ef6d
vitosandrin <sandrinvito10@gmail.com>
2024-01-20 00:51:29 -0300

    Add link to checkout page in Cart component

--- src/components/layout/Header/components/Cart/Cart.tsx
+++ src/components/layout/Header/components/Cart/Cart.tsx
@@ -2,13 +2,16 @@ import { FaCartShopping } from "react-icons/fa6";
 import * as CartStyles from "./styles";
 import { CartContext } from "@src/context/cart-context";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 export const Cart = () => {
   const { cart } = useContext(CartContext);
 
   return (
     <CartStyles.Container>
-      <FaCartShopping size={30} />
-      <CartStyles.TotalCart>{cart.total}</CartStyles.TotalCart>
+      <Link to="/checkout">
+        <FaCartShopping size={30} />
+        <CartStyles.TotalCart>{cart.total}</CartStyles.TotalCart>
+      </Link>
     </CartStyles.Container>
   );
 };
