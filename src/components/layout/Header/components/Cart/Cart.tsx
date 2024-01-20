75aad96f722aca0bec176f215e1699edab093be6
vitosandrin <sandrinvito10@gmail.com>
2024-01-20 00:09:57 -0300

    feat: cart on header component

---
+++ src/components/layout/Header/components/Cart/Cart.tsx
@@ -0,0 +1,14 @@
+import { FaCartShopping } from "react-icons/fa6";
+import * as CartStyles from "./styles";
+import { CartContext } from "@src/context/cart-context";
+import { useContext } from "react";
+export const Cart = () => {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <CartStyles.Container>
+      <FaCartShopping size={25} />;
+      <CartStyles.TotalCart>{cart.total}</CartStyles.TotalCart>
+    </CartStyles.Container>
+  );
+};
---
+++ src/components/layout/Header/components/Cart/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+export const TotalCart = styled.div`
+  position: absolute;
+  top: -8px;
+  right: -5px;
+  background: red;
+  border-radius: 100%;
+  padding: 2px 3px;
+  color: white;
+  font-size: 11px;
+`;
