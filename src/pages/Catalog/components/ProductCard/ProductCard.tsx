42d974a6ef4777a2d42fc83038b2ca44d56aa4c9
vitosandrin <sandrinvito10@gmail.com>
2024-01-20 00:51:13 -0300

    Refactor ProductCard component and styles

--- src/pages/Catalog/components/ProductCard/ProductCard.tsx
+++ src/pages/Catalog/components/ProductCard/ProductCard.tsx
@@ -1,33 +1,22 @@
-import { Button, FlexBox, Text } from "@src/components";
-import { CartContext } from "@src/context/cart-context";
+import { Text } from "@src/components";
 import { Product } from "@src/entities/models/product";
-import { useContext } from "react";
 import * as ProductCardStyles from "./styles";
-import { formatMonetaryEn } from "@src/utils/object/monetary";
+import { ProductCardActions } from "./components/ProductCardActions";
+import { ProductCardInfo } from "./components/ProductCardInfo";
+
 interface ProductCardProps {
   product: Product;
 }
 export const ProductCard = ({ product }: ProductCardProps) => {
-  const { addToCart } = useContext(CartContext);
   return (
     <ProductCardStyles.Card
       align="center"
-      justify="center"
+      justify="space-evenly"
       direction="column"
       gap="xxs"
     >
-      <Text size="medium" weight={500}>
-        {product.title}
-      </Text>
-
-      <Text size="small" weight={500}>
-        {formatMonetaryEn(product.price)}
-      </Text>
-
-      <Button colorType="Success" onClick={() => addToCart(product)}>
-        Add to cart
-      </Button>
-
+      <ProductCardInfo product={product} />
+      <ProductCardActions product={product} />
       <Text size="small">{`Available since: ${product.included_at}`}</Text>
     </ProductCardStyles.Card>
   );
--- src/pages/Catalog/components/ProductCard/styles.ts
+++ src/pages/Catalog/components/ProductCard/styles.ts
@@ -2,13 +2,24 @@ import { FlexBox } from "@src/components";
 import { styled } from "styled-components";
 
 export const Card = styled(FlexBox)`
-  background-color: ${({ theme }) => theme.colors.types.ghost};
   border-radius: 10px;
-  padding: 0.225rem;
+  padding: 0.5rem;
+
+  max-width: 425px;
+  width: 90%;
+  
+  background-color: ${({ theme }) => theme.font.colors[3]};
   box-shadow: 9px 13px 12px -3px rgba(0, 0, 0, 0.1);
-  max-width: 90%;
-`;
+
+  `;
 
 export const HeaderCard = styled(FlexBox)``;
 
 export const InfoCard = styled(FlexBox)``;
+
+export const ImageCard = styled.img`
+  max-width: 100%;
+  width: 200px;
+  display: block;
+  transform: scale(0.8);
+`;
