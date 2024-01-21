61ef7b83870c0cc25c3a8656f903774f98f62f2a
vitosandrin <sandrinvito10@gmail.com>
2024-01-21 17:45:55 -0300

    Refactor ListProductsOnCart component and add styles

--- src/pages/Checkout/components/ListProductsOnCart/ListProductsOnCart.tsx
+++ src/pages/Checkout/components/ListProductsOnCart/ListProductsOnCart.tsx
@@ -1,1 +1,11 @@
-export const ListProductsOnCart = () => {}
+import * as ListProductsOnCartStyles from "./styles";
+
+export const ListProductsOnCart = () => {
+  return (
+    <ListProductsOnCartStyles.Container
+      align="center"
+      justify="center"
+      direction="column"
+    ></ListProductsOnCartStyles.Container>
+  );
+};
