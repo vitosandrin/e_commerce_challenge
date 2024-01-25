157dc2fef36ecd5de05494882b4d7629a6d8cd78
vitosandrin <sandrinvito10@gmail.com>
2024-01-25 01:00:38 -0300

    fix: styles and add ToastMessage Component on finalize purchase

--- src/pages/Checkout/components/InfoCart/InfoCart.tsx
+++ src/pages/Checkout/components/InfoCart/InfoCart.tsx
@@ -1,23 +1,52 @@
-import { Text } from "@src/components";
+import { Button, Text, ToastMessage } from "@src/components";
 import * as InfoCartStyles from "./styles";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "@src/context/cart-context";
 import { formatMonetaryEn } from "@src/utils/object/monetary";
 
 export const InfoCart = () => {
-  const { cart } = useContext(CartContext);
+  const {
+    cart: { total, amount },
+    finalizePurchase,
+  } = useContext(CartContext);
+  const [toastMessage, setToastMessage] = useState<string | null>(null);
+
+  const showToast = (message: string) => {
+    setToastMessage(message);
+  };
+
+  const closeToast = () => {
+    setToastMessage(null);
+  };
+
+  const handleFinalizePurchase = () => {
+    showToast("Congratulations! Your purchase has been completed.");
+    finalizePurchase();
+  };
+
   return (
     <InfoCartStyles.Container
       align="center"
       justify="center"
       direction="column"
+      gap="xs"
     >
       <Text size="sm" color={1} weight={700}>
-        Total items: {cart.total}
+        Total items in cart: {total}
       </Text>
       <Text size="sm" color={1} weight={700}>
-        Amount: {formatMonetaryEn(cart.amount)}
+        Subtotal: {formatMonetaryEn(amount)}
       </Text>
+      <Button
+        colorType="Success"
+        onClick={handleFinalizePurchase}
+        disabled={total === 0}
+      >
+        <Text size="md">Finalize Purchase</Text>
+      </Button>
+      {toastMessage && (
+        <ToastMessage message={toastMessage} onClose={closeToast} />
+      )}
     </InfoCartStyles.Container>
   );
 };
